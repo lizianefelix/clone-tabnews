731fbdfebe289d357fb79c24b89c0716672d52f8
Liziane Felix <liziane.felix@hotmail.com>
2023-12-05 23:19:10 +0000

    registra `objetivo` do projeto utilizando `ideia McDonalds`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>ainda não é o tempo!</h1>;
+  return (
+    <h1>
+      projeto para registrar e praticar conhecimentos que estou aprendendo em
+      programação; Que pode também ajudar outros devs que estão querendo
+      entender algum assunto compartilhado.
+    </h1>
+  );
 }
 
 export default Home;
